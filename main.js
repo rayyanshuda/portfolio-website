1c3a82c44c9aa3170ef9f4a38b43c5b936913ec9
Rayyan Huda <139174957+rayyanshuda@users.noreply.github.com>
2025-01-08 00:44:15 -0800

    smoother transition

--- main.js
+++ main.js
@@ -72,3 +72,13 @@ window.addEventListener("scroll", function() {
     }
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 });
+
+// Get the button:
+let mybutton = document.getElementById("logo");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  behavior: 'smooth';
+}
